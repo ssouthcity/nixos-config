216206e63edfe48f8c01d8258d9ee85998c4372a
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2024-05-26 01:49:11 +0200

    enabled hyprland

--- users/southcity/home.nix
+++ users/southcity/home.nix
@@ -2,14 +2,13 @@
 
 {
   imports = [
-    inputs.catppuccin.homeManagerModules.catppuccin
-    inputs.hyprland.homeManagerModules.default
     ../../modules/home-manager/catppuccin.nix
     ../../modules/home-manager/direnv.nix
     ../../modules/home-manager/discord.nix
     ../../modules/home-manager/fzf.nix
     ../../modules/home-manager/git.nix
     ../../modules/home-manager/gtk.nix
+    ../../modules/home-manager/hypr
     ../../modules/home-manager/kitty.nix
     ../../modules/home-manager/nvim.nix
     ../../modules/home-manager/starship.nix
@@ -17,6 +16,8 @@
     ../../modules/home-manager/xdg.nix
     ../../modules/home-manager/zotero.nix
     ../../modules/home-manager/zsh.nix
+    inputs.catppuccin.homeManagerModules.catppuccin
+    inputs.hyprland.homeManagerModules.default
   ];
 
   config = {
