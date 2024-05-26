a99c5f2b7487d38f55a66378dea4dfe89b3102a0
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2024-05-26 23:40:21 +0200

    gruvbox gtk theme

--- modules/home-manager/gtk.nix
+++ modules/home-manager/gtk.nix
@@ -2,11 +2,23 @@
 {
   gtk = {
     enable = true;
-    catppuccin = {
-      enable = true;
-      cursor.enable = true;
-      icon.enable = true;
-      tweaks = [ "normal" "rimless" ];
+    # catppuccin = {
+    #   enable = true;
+    #   cursor.enable = true;
+    #   icon.enable = true;
+    #   tweaks = [ "normal" "rimless" ];
+    # };
+    theme = {
+      name = "Gruvbox-Dark-B";
+      package = pkgs.gruvbox-gtk-theme;
+    };
+    iconTheme = {
+      name = "Gruvbox-Plus-Dark";
+      package = pkgs.gruvbox-plus-icons;
+    };
+    cursorTheme = {
+      name = "Capitaine Cursors (Gruvbox)";
+      package = pkgs.capitaine-cursors-themed;
     };
   };
 
