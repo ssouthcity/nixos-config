33dfe6977c51df8915e0e3c7fb9aaf978888f4ab
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2024-06-30 12:31:43 +0000

    organized cli tools

---
+++ modules/home-manager/cli/default.nix
@@ -0,0 +1,14 @@
+{
+  imports = [
+    ./editors/nixvim
+    ./multiplexers/tmux.nix
+    ./programming/go.nix
+    ./prompts/starship.nix
+    ./shells/zsh.nix
+    ./terminals/kitty.nix
+    ./vscs/git.nix
+    ./direnv.nix
+    ./fzf.nix
+    ./jq.nix
+  ];
+}
--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -3,22 +3,14 @@
     ./audio/pulseaudio.nix
     ./browsers/chrome.nix 
     ./browsers/firefox.nix 
-    ./editor/nixvim
     ./gaming/retroarch
     ./hypr
     ./music/spotify
-    ./programming
 
-    ./direnv.nix
     ./discord.nix
-    ./fzf.nix
-    ./git.nix
-    ./jq.nix
-    ./kitty.nix
-    ./starship.nix
-    ./tmux.nix
     ./xdg.nix
     ./zotero.nix
-    ./zsh.nix
+
+    ./cli
   ];
 }
